87ffb13da9ea0ae1ad577cbd76f9513a72e0de1a
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 17:03:38 -0300

    Add files via upload

---
+++ clave/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace VERIFICACIÓN_DE_UNA_CLAVE_3_OPORTUNIDADES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte K, SW;
+            string CLAVE;
+            SW = 0; K = 0;
+            do
+            {
+                Console.Write("DIGITE CLAVE: "); CLAVE = Console.ReadLine();
+                if ((CLAVE.ToUpper() == "ARIEL"))
+                {
+                    SW = 1;
+                }
+                else
+                {
+                    K++;
+                }
+            } while (((K < 3) & (SW == 0)));
+            if (SW == 1)
+            {
+                Console.WriteLine("BIENVENIDO " + CLAVE);
+            }
+            else
+            {
+                Console.WriteLine("OPORTUNIDADES TERMINADAS");
+            }
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
